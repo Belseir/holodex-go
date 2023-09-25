4b0547bb2e2c2aef7c286f90eebaf63e059247d8
FutrykFabricio <ffutryk52@gmail.com>
2023-09-25 00:29:28 -0300

    feat: add langs enum

---
+++ pkg/enums/langs.go
@@ -0,0 +1,29 @@
+package enums
+
+import "fmt"
+
+var Langs = newLangs()
+
+type LangsType string
+
+func newLangs() *langs {
+	return &langs{
+		ENGLISH:  "en",
+		JAPANESE: "jp",
+		CHINESE:  "zh",
+	}
+}
+
+type langs struct {
+	ENGLISH  LangsType
+	JAPANESE LangsType
+	CHINESE  LangsType
+}
+
+func (cs *langs) IsValid(l LangsType) (bool, error) {
+	if l != Langs.ENGLISH && l != Langs.JAPANESE && l != Langs.CHINESE {
+		return false, fmt.Errorf("Invalid Langs (%s) accessed. Expected 'en', 'jp' or 'zh'", l)
+	}
+
+	return true, nil
+}
