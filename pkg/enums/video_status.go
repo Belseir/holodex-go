296694a4d339acc831ebc760b0cbbb2b54737cea
FutrykFabricio <ffutryk52@gmail.com>
2023-09-25 00:30:03 -0300

    feat: add video status enum

---
+++ pkg/enums/video_status.go
@@ -0,0 +1,39 @@
+package enums
+
+import "fmt"
+
+var VideoStatus = newVideoStatus()
+
+type VideoStatusType string
+
+func newVideoStatus() *videoStatus {
+	return &videoStatus{
+		NEW:      "new",
+		UPCOMING: "upcoming",
+		LIVE:     "live",
+		PAST:     "past",
+		MISSING:  "missing",
+	}
+}
+
+type videoStatus struct {
+	NEW      VideoStatusType
+	UPCOMING VideoStatusType
+	LIVE     VideoStatusType
+	PAST     VideoStatusType
+	MISSING  VideoStatusType
+}
+
+func (v *videoStatus) IsValid(vs VideoStatusType) (bool, error) {
+	switch vs {
+	case
+		VideoStatus.NEW,
+		VideoStatus.UPCOMING,
+		VideoStatus.LIVE,
+		VideoStatus.PAST,
+		VideoStatus.MISSING:
+		return true, nil
+	default:
+		return false, fmt.Errorf("Invalid VideoStatus (%s) accessed", vs)
+	}
+}
